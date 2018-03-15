5b89a174b253022161229ac77e665734e8dfa180
Nabyl Bennouri <nbennouri@car.fit>
2018-03-15 15:57:09 -0700

    Human API token exchange

---
+++ functions/README.md
@@ -0,0 +1,18 @@
+
+# Humantiv Backend
+
+Cloud functions supporting the Humantiv app.
+
+## Installation
+
+```
+cd functions
+npm install
+```
+
+## Functions List
+
+**humanAPITokenExchange**: 
+
+ - *Input*: Human API user sessionTokenObject.
+ - *Returns*:  Human API user publicToken
---
+++ functions/index.js
@@ -0,0 +1,41 @@
+const functions = require('firebase-functions');
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+const request = require('request');
+
+admin.initializeApp(functions.config().firebase);
+
+//Endpoint for specified 'authURL'
+exports.humanAPITokenExchange = functions.https.onRequest((req, res) => {
+
+    console.log(`humanAPITokenExchange = ${JSON.stringify(req.body)}`);
+    var sessionTokenObject = req.body;
+    sessionTokenObject.clientSecret = '1de96f660418ba961d6f2de259f01aaed5da3445';
+  
+    request({
+      method: 'POST',
+      uri: 'https://user.humanapi.co/v1/connect/tokens',
+      json: sessionTokenObject
+    }, (err, resp, body) => {
+        if(err) {
+            console.log(`error = ${err}`);
+            return res.send(422);
+        }
+  
+         //Use these values to determine which user launched Connect
+        console.log("clientId ="+ body.clientId);
+  
+        /* Human API credentials. 
+        Save these with the user model in your system. */
+        console.log("humanId = " + body.humanId);
+        console.log("accessToken = "+ body.accessToken);
+        console.log("publicToken = "+ body.publicToken);
+  
+  
+        //Send back publicToken to app
+        var responseJSON = {publicToken: body.publicToken};
+  
+        res.setHeader('Content-Type', 'application/json');
+        res.status(201).send(JSON.stringify(responseJSON));
+      });
+  });
